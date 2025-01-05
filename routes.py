53ebc83d9cefb51b488b178c370aaeefdf6db9d0
Julio <julioscp2009@hotmail.com>
2025-01-05 22:01:29 +0000

    Adicionado o Pandas para ler e tratar os dados do CSV

--- routes.py
+++ routes.py
@@ -1,22 +1,46 @@
 # routes.py
 from flask import Blueprint, request, jsonify
+import pandas as pd
+from io import StringIO
 
 # Criando um Blueprint para as rotas
 routes = Blueprint('routes', __name__)
 
-@routes.route('/',methods=['GET'])
+@routes.route('/', methods=['GET'])
 def home():
     return jsonify({"message": "Bem-vindo à API Flask! tetse"})
 
-@routes.route('/processar-extrato')
-def processar_arquivo():
-    dados = request.get_json()  # Pega os dados enviados no corpo da solicitação
-    if not dados:
-        return jsonify({"error": "Nenhum dado enviado!"}), 400
+# Função para sanitizar valores monetários
+def sanitizar_valor(valor):
+    if isinstance(valor, str):  # Verifica se é uma string
+        valor = valor.replace("R$", "").replace("\u00a0", "").replace(".", "").replace(",", ".").strip()
+        return float(valor)
+    return valor
 
-    # Processa os dados recebidos
-    resposta = {
-        "message": "Dados recebidos com sucesso!",
-        "dados_recebidos": dados
-    }
-    return jsonify(resposta)
+@routes.route('/processar-extrato', methods=['POST'])
+def processar_arquivo():
+     # Verifica se um arquivo foi enviado
+    if 'file' not in request.files:
+        return jsonify({'error': 'Arquivo não encontrado'}), 400
+
+    file = request.files['file']  # O arquivo enviado
+
+    # Lê o arquivo como texto
+    file_data = file.stream.read().decode('utf-8')
+
+    # Converte para um DataFrame usando pandas
+    csv_data = pd.read_csv(StringIO(file_data))
+
+     # Sanitiza a coluna "Valor" (ou a coluna que você precisa)
+    if 'Valor' in csv_data.columns:
+        csv_data['Valor'] = csv_data['Valor'].apply(sanitizar_valor)
+
+    # Exemplo: Retorna as 5 primeiras linhas do CSV em JSON
+    data_csv = csv_data.to_dict(orient='records')
+
+    # Exemplo de processamento
+    return jsonify({
+        'message': 'Arquivo recebido e processado com sucesso!',
+        'filename': file.filename,
+        'dados': data_csv
+    }), 200

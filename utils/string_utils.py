ad62073c37835dd192c8780d5083a54da2bd1a66
Julio <julioscp2009@hotmail.com>
2025-01-12 22:15:40 +0000

    Adicionado funcoes uteis

--- utils/string_utils.py
+++ utils/string_utils.py
@@ -11,3 +11,17 @@ def remover_acentos(texto):
 def converter_para_maiusculas(texto):
     return texto.upper()
 
+def converter_para_minusculas(texto):
+    return texto.lower()
+
+def capitalize_primeira_letra(texto):
+    return texto.capitalize()
+
+def remover_espacos_extras(texto):
+    return " ".join(texto.split())
+
+def esta_vazia(texto):
+    return not texto or texto.strip() == ""
+
+def extrair_substring(texto, inicio, fim):
+    return texto[inicio:fim]
